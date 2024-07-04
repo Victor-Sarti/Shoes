2e2d2aec1b3eb18f32e468f06ca05364029cdb59
victor-sarti <sartivictor24@gmail.com>
2024-07-04 13:22:10 -0300

    import app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar";
+import Section from "./Section";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>,
+    <Section/>
+    </div>
+   
+   
+  );
+};
+
+export default App
