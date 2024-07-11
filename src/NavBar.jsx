e563c35dc5a7278fa16c9258016541400c21434e
victor-sarti <sartivictor24@gmail.com>
2024-07-11 17:06:43 -0300

    fix: ajusted gap navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,7 +1,7 @@
 import logo2 from "./assets/logo2.png"
 const NavBar = () => {
     return (
-        <header className=" z-10 fixed left-2/4 -translate-x-1/2 flex items-center gap-10">
+        <header className=" z-10 fixed left-2/4 -translate-x-1/2 flex items-center gap-28">
             <img className="w-20" src={logo2} alt="logo" />
             <ul className=" grid grid-cols-3">
                 <li>Home</li>
