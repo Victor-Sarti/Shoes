b711f027df4e1df0bd69a7912717eddd5666e84e
victor-sarti <sartivictor24@gmail.com>
2024-08-01 21:59:30 -0300

    import styles.css

--- src/Section.jsx
+++ src/Section.jsx
@@ -4,14 +4,16 @@ import blue from "./assets/blue.png"
 import yellow from "./assets/yellow.png"
 import arrowR from "./assets/arrowR.svg"
 import arrowL from "./assets/arrowL.svg"
+import './styles.css'
+
 
 const Section = () => {
     return (
         <section className="  h-screen relative">
             <div className=" w-3/4 h-full m-auto relative">
-                <div className=" absolute inset-0">
+                <div className=" absolute inset-0  slide-in">
                     <div className=" absolute w-3/5 translate-y-1/3 ">
-                         <img className=" w-full  translate-x-1/4 -rotate-[30deg]  " src={cherry} alt="dunk" />
+                         <img className=" w-full  translate-x-1/4 -rotate-[30deg]  rot-car " src={cherry} alt="dunk " />
                     </div>
                     
                 </div>
@@ -19,14 +21,15 @@ const Section = () => {
 
                 <div className=" absolute h-full w-3/4 right-24 flex flex-col items-end  justify-end gap-5">
                    
-                    <p className="  font-semibold"> Nike</p>
+                    <p className=" enter0  font-semibold"> Nike</p>
 
-                    <h2 className=" text-8xl "> Cherry Dunk</h2>
+                    <h2 className=" enter1 text-7xl"> Cherry Dunk</h2>
 
-                    <p className=" text-neutral-500 text-sm text-right max-w-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem placeat non odit delectus hic eveniet quos debitis est! Dolore expedita delectus temporibus labore hic placeat.</p>
+                    <p className="enter2 text-neutral-500  text-sm text-right max-w-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem placeat non odit delectus hic eveniet quos debitis est! Dolore expedita delectus temporibus labore hic placeat.</p>
 
                     <button  className=" uppercase border-2  border-green-600 mb-10 w-32 h-9 bg-lime-400 rounded-lg text-black pointer"> Saiba Mais</button>
                 </div>
+               
             </div>
 
             <div className=" absolute w-[70vw] top-2/4 left-2/4  -translate-x-2/4  -translate-y-6 flex justify-between ">
@@ -37,8 +40,8 @@ const Section = () => {
             <div className=" absolute top-2/4 left-2/4 h-48 w-[90vw] -translate-x-2/4 flex flex-col justify-end gap-4">
                 <div className=" text-5xl font-semibold"> 01 </div>
                 <ul className=" flex gap-4">
-                    <li className="w-12 h-1  bg-lime-400 border rounded-md duration-75 " ></li>
-                    <li className=" w-12 h-1  bg-lime-400 border rounded-md duration-75" ></li>
+                    <li className="w-12 h-1  border rounded-md duration-75 active: bg-white " ></li>
+                    <li className=" w-12 h-1  bg-lime-400 border rounded-md duration-75 " ></li>
                     <li className=" w-12 h-1  bg-lime-400 border rounded-md duration-75" ></li>
                     
                 </ul>
